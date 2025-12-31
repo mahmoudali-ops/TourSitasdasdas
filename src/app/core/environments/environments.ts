0b98d023291edb79a9b3d2c6b9c16cf1fc210a7b
mahmoudali-ops <mahmoua487@gmail.com>
2025-12-31 17:31:10 +0200

    sd

--- src/app/core/environments/environments.ts
+++ src/app/core/environments/environments.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  BaseUrl: 'https://tourpro.runasp.net',  //https://tourpro.runasp.net
+  BaseUrl: 'https://tourpro.runasp.net',   //https://tourpro.runasp.net
     UrlServer:'http://localhost:4200'
  };
- 
