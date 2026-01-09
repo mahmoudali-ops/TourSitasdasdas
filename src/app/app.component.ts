3e09f281aaf0f2332d13747c1f936a8d2985ac49
mahmoudali-ops <mahmoua487@gmail.com>
2026-01-09 02:31:10 +0200

    fdg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'TourSiteClient';
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe(() => {
+          const fbq = (window as any)['fbq'];
+          if (typeof fbq === 'function') {
+            fbq('track', 'PageView');
+          }
+        });
+    }
+  }
+
 }
